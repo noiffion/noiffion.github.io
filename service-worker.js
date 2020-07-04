003845a6393e67280e866cc5241d9d6bd50383a5
David Kerekes <davkedav@gmail.com>
2020-07-04 16:02:55 +0200

    manifest.json added

--- service-worker.js
+++ service-worker.js
@@ -1,32 +1,27 @@
 const cacheName = 'cache-v1';
 const precacheResources = [
   '/',
-  'index.html',
-  'style.css',
-  'script.js',
+  '/index.html',
+  '/public/styles/style.css',
+  '/public/scripts/script.js',
+  '/public/images/apple-touch-icon.png'
 ];
 
 self.addEventListener('install', event => {
   console.log('Service worker install event!');
   event.waitUntil(
     caches.open(cacheName)
-      .then(cache => {
-        return cache.addAll(precacheResources);
-      })
+      .then(cache => cache.addAll(precacheResources))
   );
 });
 
-self.addEventListener('activate', event => {
-  console.log('Service worker activate event!');
-});
+self.addEventListener('activate', event => console.log('Service worker activate event!'));
 
 self.addEventListener('fetch', event => {
   console.log('Fetch intercepted for:', event.request.url);
   event.respondWith(caches.match(event.request)
     .then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
+        if (cachedResponse) return cachedResponse;
         return fetch(event.request);
       })
     );
